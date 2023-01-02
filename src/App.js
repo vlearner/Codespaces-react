3ae1c27f5768fa5d8d260ddf8372f366d5886fc4
Vishal Barot <vlearner@users.noreply.github.com>
2023-01-02 04:09:07 +0000

    Githhub Codespace

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
+          <br></br>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -20,8 +15,12 @@ function App() {
           >
             Learn React
           </a>
-        </p>
+          <img src="Octocat.png" className="App-logo" alt="logo" />
+       </p>
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
